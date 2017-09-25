2f8a0957ba598e184ce7ec8b263d551ee1a69558
Timothy Meixell <misternu@gmail.com>
2017-09-25 11:52:09 -0500

    Complete problem 1

---
+++ 1/benchmark.rb
@@ -0,0 +1,12 @@
+require 'benchmark'
+require_relative 'solution'
+
+# much larger number, for the sake of benchmarking
+n = 10000000
+
+Benchmark.bm do |x|
+  puts "Iterative solution"
+  x.report { reduce_three_and_five(n) }
+  puts "Number theory solution"
+  x.report { math_three_and_five(n) }
+end
---
+++ 1/solution.rb
@@ -0,0 +1,22 @@
+## Common Iterative Sense Solution:
+
+def reduce_three_and_five(n)
+  (1...n).reduce(0) { |m, n| n % 3 == 0 || n % 5 == 0 ? m + n : m }
+end
+
+## Number Theory Solution:
+
+def integer_sum(n)
+  n * (n + 1) / 2
+end
+
+def multiples_sum(n, l)
+  m = l/n # The number of multiples of n up to and including l
+  n * integer_sum(m)
+end
+
+def math_three_and_five(n)
+  multiples_sum(3, n-1) + # multiples of 3 plus
+  multiples_sum(5, n-1) - # multiples of 5 minus
+  multiples_sum(15, n-1) # multiples of 15 (duplicates)
+end
