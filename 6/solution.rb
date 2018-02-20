00d9a093723667431606ffc34131fdb3e2c147f0
Timothy Meixell <misternu@gmail.com>
2018-02-20 14:24:02 -0600

    Solve problem 6 in O(1)

--- 6/solution.rb
+++ 6/solution.rb
@@ -1,4 +1,10 @@
 def solution(max)
+  square_of_sum = ((max * (max + 1)) / 2)**2
+  sum_of_squares = ((2 * max + 1) * (max + 1)) / 6
+  square_of_sum - sum_of_squares
+end
+
+def linear_solution(max)
   sum_of_squares = (1..max).map do |n|
     n**2
   end .reduce(&:+)
